9b1aa6ee63f44a7131ed4659830e4a1ce2403ed3
JerylLim <jeryllimacc@gmail.com>
2022-01-07 10:19:31 +1100

    Updates functions to let user select data interval

---
+++ .ipynb_checkpoints/main-checkpoint.py
@@ -0,0 +1,15 @@
+import fetch-data as fd
+
+
+def main():
+    
+    # Request crypto tickers from user, and record as list
+    ticker_list = fd.input_cryptos()
+    
+    # Fetch data from yfinance for each ticker, and create pandas dataframe
+    portfolio_df = fd.get_ticker_data(ticker_list)
+
+    
+if __name__ == '__main__':
+    main()
+    
