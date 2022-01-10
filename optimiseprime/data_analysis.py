a6e15ed452caa1b77b239fbd0f8e0c52efdc317d
JerylLim <jeryllimacc@gmail.com>
2022-01-11 09:07:07 +1100

    Adds functions to calculate portfolio weights and amount allocation based on r-r ratios

--- optimiseprime/data_analysis.py
+++ optimiseprime/data_analysis.py
@@ -61,7 +61,7 @@ def calculate_gain_pain_ratio(ticker_list, portfolio_df):
     return g2p_series
     
 
-def calculate_proportion_score(ratios_df):
+def calculate_weights(ratios_df):
     '''
     Converts the risk-reward ratios of each cryptocurrency to a proportion score, by dividing the ratio value against the sum of ratio values in that column. 
     If any value is <0, winsorize to 0. This would create a proportion score of 0, indicating that no weight should be allocated to the corresponding asset. 
@@ -70,35 +70,121 @@ def calculate_proportion_score(ratios_df):
     ratios_df(pandas.DataFrame): A pandas dataframe containing risk-reward ratios of user specified cryptocurrencies. 
     
     '''
-    ratios_prop_score = {}
+    weights = {}
     # Winsorize negative values to 0
     ratios_df[ratios_df < 0] = 0
     for column in ratios_df:
-        ratios_prop_score[column] = ratios_df[column] / sum(ratios_df[column])
-    ratios_prop_score = pd.DataFrame(ratios_prop_score)
-    return ratios_prop_score
+        weights[column] = ratios_df[column] / sum(ratios_df[column])
+    weights = pd.DataFrame(weights)
+    return weights
 
-def sharpe_portfolio(sharpe_ratios, ratio_prop_score, portfolio_value):
+def sharpe_portfolio(ratios_df, weights, portfolio_value):
     '''
     Calculates the recommended portfolio allocations based off a set of given cryptocurrency 'proportion scores'
     Proportion scores are calculated from sharpe ratios.
     
     Parameters:
-    sharpe_ratios(pd.Series): A Series object with sharpe ratios of user-specified cryptocurrencies
-    ratio_prop_score(pd.DataFrame): A pd.DataFrame object with proportion scores for each user-specified cryptocurrency, calculated from the corresponding risk-reward metrics
+    ratios_df(pd.DataFrame): A pd.DataFrame with financial risk-reward ratios of user-specified cryptocurrencies
+    weights(pd.DataFrame): A pd.DataFrame object with calculated weights for each user-specified cryptocurrency, according to the corresponding risk-reward metrics
     portfolio_value(float): The total value of the user portfolio
     '''
     print("\nMetric: Sharpe Ratio")
     print("--------------")
+    sharpe_ratios = ratios_df['sharpe'].sort_values(ascending = False)
     sharpe_ratios.sort_values(ascending = False, inplace = True)
-    sharpe_recommended_amt = {}
+    sharpe_allocation = {}
     # Print Sharpe ratios and recommended portfolio weightings.
     for ticker, ratio in sharpe_ratios.iteritems():
         print(f"{ticker}: {ratio:.2f}")
-        print(f"Recommended percentage of total portfolio: {ratio_prop_score.loc[ticker][0]*100:.2f}%")
-        
+        print(f"Recommended percentage of total portfolio: {weights.loc[ticker][0]*100:.2f}%")
+
         # Record sharpe ratios as key-value pairs in a dict
-        sharpe_recommended_amt[ticker] = portfolio_value * ratio_prop_score.loc[ticker][0]
-        print(f"Recommended value allocation = ${sharpe_recommended_amt[ticker]:.2f}\n")
-    # Convert sharpe_recommended_amt dict to pd.Series
-    sharpe_recommended_amt = pd.Series(sharpe_recommended_amt)
+        sharpe_allocation[ticker] = portfolio_value * weights.loc[ticker][0]
+        print(f"Recommended value allocation = ${sharpe_allocation[ticker]:.2f}\n")
+    # Convert sharpe_allocation dict to pd.Series
+    sharpe_allocation= pd.Series(sharpe_allocation)
+    return sharpe_allocation
+
+
+def sortino_portfolio(ratios_df, weights, portfolio_value):
+    '''
+    Calculates the recommended portfolio allocations based off a set of given cryptocurrency 'proportion scores'
+    Proportion scores are calculated from sortino ratios.
+    
+    Parameters:
+    ratios_df(pd.DataFrame): A pd.DataFrame with financial risk-reward ratios of user-specified cryptocurrencies
+    weights(pd.DataFrame): A pd.DataFrame object with calculated weights for each user-specified cryptocurrency, according to the corresponding risk-reward metrics
+    portfolio_value(float): The total value of the user portfolio
+    '''
+    print("\nMetric: Sortino Ratio")
+    print("--------------")
+    sortino_ratios = ratios_df['sortino'].sort_values(ascending = False)
+    sortino_ratios.sort_values(ascending = False, inplace = True)
+    sortino_allocation = {}
+    # Print sortino ratios and recommended portfolio weightings.
+    for ticker, ratio in sortino_ratios.iteritems():
+        print(f"{ticker}: {ratio:.2f}")
+        print(f"Recommended percentage of total portfolio: {weights.loc[ticker][0]*100:.2f}%")
+
+        # Record sortino ratios as key-value pairs in a dict
+        sortino_allocation[ticker] = portfolio_value * weights.loc[ticker][0]
+        print(f"Recommended value allocation = ${sortino_allocation[ticker]:.2f}\n")
+    # Convert sortino_allocation dict to pd.Series
+    sortino_allocation= pd.Series(sortino_allocation)
+    return sortino_allocation
+
+
+def adj_sortino_portfolio(ratios_df, weights, portfolio_value):
+    '''
+    Calculates the recommended portfolio allocations based off a set of given cryptocurrency 'proportion scores'
+    Proportion scores are calculated from adj sortino ratios.
+    
+    Parameters:
+    ratios_df(pd.DataFrame): A pd.DataFrame with financial risk-reward ratios of user-specified cryptocurrencies
+    weights(pd.DataFrame): A pd.DataFrame object with calculated weights for each user-specified cryptocurrency, according to the corresponding risk-reward metrics
+    portfolio_value(float): The total value of the user portfolio
+    '''
+    print("\nMetric: Adjusted Sortino Ratio")
+    print("--------------")
+    adj_sortino_ratios = ratios_df['adj_sortino'].sort_values(ascending = False)
+    adj_sortino_ratios.sort_values(ascending = False, inplace = True)
+    adj_sortino_allocation = {}
+    # Print adj sortino ratios and recommended portfolio weightings.
+    for ticker, ratio in adj_sortino_ratios.iteritems():
+        print(f"{ticker}: {ratio:.2f}")
+        print(f"Recommended percentage of total portfolio: {weights.loc[ticker][0]*100:.2f}%")
+
+        # Record adj sortino ratios as key-value pairs in a dict
+        adj_sortino_allocation[ticker] = portfolio_value * weights.loc[ticker][0]
+        print(f"Recommended value allocation = ${adj_sortino_allocation[ticker]:.2f}\n")
+    # Convert adj_sortino_allocation dict to pd.Series
+    adj_sortino_allocation= pd.Series(adj_sortino_allocation)
+    return adj_sortino_allocation
+
+
+def gain_pain_portfolio(ratios_df, weights, portfolio_value):
+    '''
+    Calculates the recommended portfolio allocations based off a set of given cryptocurrency 'proportion scores'
+    Proportion scores are calculated from gain-to-pain ratios.
+    
+    Parameters:
+    ratios_df(pd.DataFrame): A pd.DataFrame with financial risk-reward ratios of user-specified cryptocurrencies
+    weights(pd.DataFrame): A pd.DataFrame object with calculated weights for each user-specified cryptocurrency, according to the corresponding risk-reward metrics
+    portfolio_value(float): The total value of the user portfolio
+    '''
+    print("\nMetric: Gain-to-Pain Ratio")
+    print("--------------")
+    gain_pain_ratios = ratios_df['gain_pain'].sort_values(ascending = False)
+    gain_pain_ratios.sort_values(ascending = False, inplace = True)
+    gain_pain_allocation = {}
+    # Print gain pain ratios and recommended portfolio weightings.
+    for ticker, ratio in gain_pain_ratios.iteritems():
+        print(f"{ticker}: {ratio:.2f}")
+        print(f"Recommended percentage of total portfolio: {weights.loc[ticker][0]*100:.2f}%")
+
+        # Record gain pain ratios as key-value pairs in a dict
+        gain_pain_allocation[ticker] = portfolio_value * weights.loc[ticker][0]
+        print(f"Recommended value allocation = ${gain_pain_allocation[ticker]:.2f}\n")
+    # Convert gain_pain_allocation dict to pd.Series
+    gain_pain_allocation= pd.Series(gain_pain_allocation)
+    return gain_pain_allocation
