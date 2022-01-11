5d2573b72df3a0ae49dd0e0c31718160504548a5
CollettaB <79147539+CollettaB@users.noreply.github.com>
2022-01-11 20:28:07 +1100

    data_prep - cleanup get_ticker_data
    Should be companitble
    Made sure it works with main

--- optimiseprime/data_prep.py
+++ optimiseprime/data_prep.py
@@ -62,7 +62,7 @@ def get_investment_amt():
     return investment_amount
         
 
-def get_ticker_data(ticker_list, years_back = 3):
+def get_ticker_data(ticker_list, years_back = 3, freq='1d'):
     ''''
     Iterates through each ticker in user portfolio and fetches OHLCV data from Yahoo Finance API into a pandas dataframe. 
     Also records daily returns for each cryptocurrency as separate columns within the same dataframe.
@@ -72,52 +72,25 @@ def get_ticker_data(ticker_list, years_back = 3):
     years_back (int): Number of years back from the current date, for which data is to be fetched. Set to 3 by default
     '''
     # Initialise dict to record OHLCV data
-    d ={}
     # Define start and end dates
     end = datetime.datetime.now()
     start = datetime.datetime.now() - datetime.timedelta(days= 365 * years_back)
     # create empty dataframe
-    cryptos_final = pd.DataFrame()
-    
-    # Get timeframe 
-    #interval_list = ['1d', '1w', '1m','1y']
-    #interval = str(input(
-    #    f"\nPlease select the interval: \n"
-    #    f"(Valid intervals: 1d, 1w, 1m, 1y)\n"
-    #)
-    #              )
+    crypto_final = pd.DataFrame()
                   
-    #while interval not in interval_list:
-    #    print("\nError: You have selected an invalid interval")
-    #    interval = str(input(
-    #        f"Please select the interval: \n"
-    #        f"(Valid intervals: 1d, 1w, 1m, 1y)\n"
-    #    )
-    #                  )
-                              
     for ticker in ticker_list:
-        try:
-            # download the crypto price 
-            crypto = yf.Ticker(ticker)
-            crypto_df = crypto.history(
-                start = start, 
-                end = end, 
-                interval = '1d'
+        # download the crypto price 
+        crypto = yf.Ticker(ticker).history(
+            start = start, 
+            end = end, 
+            interval = freq
             )
             
-            # append the individual crpyto prices 
-            if len(crypto_df) == 0:
-                None
-            else:
-                d[(ticker, "open")] = crypto_df['Open']
-                d[(ticker, "high")] = crypto_df['High']
-                d[(ticker, "low")] = crypto_df["Low"]
-                d[(ticker, "close")] = crypto_df["Close"]
-                d[(ticker, "volume")] = crypto_df["Volume"]
-                d[(ticker, "daily_return")] = crypto_df['Close'].pct_change()
-        except Exception:
-            None
+        crypto[(ticker, "daily_return")] = crypto['Close'].pct_change()
+        crypto_final = pd.concat([crypto_final, crypto], axis=1)
+        
+ 
     
-    d = pd.DataFrame(d)
-    d = d.dropna()
-    return d
+    crypto_final = crypto_final.dropna().drop(['Dividends', 'Stock Splits'], axis = 1)
+    
+    return crypto_final
